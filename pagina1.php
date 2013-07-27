cd4d8bd6bca42f10d1e2e9490ebf6da4ecd993a5
fabiodemetrio <fabio.demetrio@gmail.com>
2013-07-27 15:48:55 -0300

    second commit

---
+++ pagina1.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Problema</title>
+</head>
+<body>
+
+<?php
+       $num=rand(1,100);
+       echo $num;
+       echo "<BR>";     
+       if ($num <= 50)
+           {
+           echo "Ele é igual ou menor que 50";
+           }
+         else 
+           {
+            echo "Ele é maior que 50 ";
+           } 
+
+?>
+
+</body>
+</html>
